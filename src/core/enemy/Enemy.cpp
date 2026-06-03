60a438ac786ce8261e540bacfd2ca5a5e7a5da13
Ysée PARENTI <ysee.parenti@telecom-sudparis.eu>
2026-06-03 18:11:14 +0200

    implementation move

--- src/core/enemy/Enemy.cpp
+++ src/core/enemy/Enemy.cpp
@@ -1,19 +1,42 @@
 #include "Enemy.h"
-using namespace std;
 
-Enemy::Enemy(double maxHealth, double movementSpeed, int attackDamage, double attackRange,
-               double attackCooldown, double x, double y, int bounty)
-    : maxHealth(maxHealth), currentHealth(maxHealth), movementSpeed(movementSpeed),
-      attackDamage(attackDamage), attackRange(attackRange), attackCooldown(attackCooldown),
-      x(x), y(y), isDead(false), bounty(bounty), currentCooldown(0) {};
+#include <cmath>
 
-void Enemy::move(vector<Tile*> path) {}; //à faire
+Enemy::Enemy(double maxHealth, double movementSpeed, int attackDamage,
+             double attackRange, double attackCooldown, double x, double y,
+             int bounty, Path* beginPath)
+    : maxHealth(maxHealth),
+      currentHealth(maxHealth),
+      movementSpeed(movementSpeed),
+      attackDamage(attackDamage),
+      attackRange(attackRange),
+      attackCooldown(attackCooldown),
+      x(x),
+      y(y),
+      isDead(false),
+      bounty(bounty),
+      currentCooldown(0),
+      currentPath(beginPath) {};
 
-void Enemy::takeDamage(double damage) {
-    currentHealth -= damage;
-    if (currentHealth <= 0) {
-        isDead = true;
-    }
+void Enemy::move(const int dt) {
+  double targetX = currentPath->next()->getX() + 0.5;
+  double targetY = currentPath->next()->getY() + 0.5;
+
+  double moveDistance = movementSpeed * dt;
+  double distanceToTarget = sqrt(pow(targetX - x, 2) + pow(targetY - y, 2));
+
+  double ratio = moveDistance / distanceToTarget;
+  x += (targetX - x) * ratio;
+  y += (targetY - y) * ratio;
+
+  if ((int)x == targetX && (int)y == targetY) {
+    currentPath = currentPath->next();
+  };
 }
 
-
+  void Enemy::takeDamage(double damage) {
+    currentHealth -= damage;
+    if (currentHealth <= 0) {
+      isDead = true;
+    }
+  }
--- src/core/enemy/Enemy.h
+++ src/core/enemy/Enemy.h
@@ -2,31 +2,32 @@
 #include <vector>
 
 #include "../map/Tile.h"
+#include "../map/Path.h"
 #include "Attack.h"
 
 class Enemy {
  protected:
   double maxHealth;
   double currentHealth;
-  double movementSpeed;
+  double movementSpeed; //Tile/ms
   int attackDamage;
   double attackRange;
   double attackCooldown;
   double x;
   double y;
-  std::unique_ptr<Tile> currentTile;
+  Path* currentPath; 
   bool isDead;
   int bounty;              // pièces gagnées à la mort de l'ennemi
-                           // visuel a mettre texture ?
+  // visuel a mettre texture ?
   double currentCooldown;  // temps restant avant de pouvoir attaquer à nouveau
 
  public:
   Enemy(double maxHealth, double movementSpeed, int attackDamage,
         double attackRange, double attackCooldown, double x, double y,
-        int bounty);
+        int bounty, Path* beginPath);
   virtual ~Enemy() = default;
 
-  void move(std::vector<Tile*> path);
+  void move(const int dt);
   virtual void attack() = 0;
   void takeDamage(double damage);
   bool isAlive() const { return !isDead; };
