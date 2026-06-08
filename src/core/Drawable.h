9d7ab177ad196e722025032ff3b2e7560f8fcbfa
Ferreira Maxence <maxence.ferreira@telecom-sudparis.eu>
2026-06-08 16:45:17 +0200

    drawable sync

---
+++ src/core/Drawable.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+
+struct context
+{
+	unsigned int dt;
+	double offsetX, offsetY;
+	sf::RenderWindow* window;
+};
+
+class Drawable
+{
+public:
+	virtual ~Drawable() = default;
+	virtual void draw(const context& ctx)=0;
+};
--- src/core/common.h
+++ src/core/common.h
@@ -2,11 +2,4 @@
 #define TILE_SIZE 50.f
 #define SCREEN_WIDTH 1200u
 #define SCREEN_HEIGHT 800u
-#include "SFML/Graphics.hpp"
-
-struct context
-{
-	unsigned int dt;
-	double offsetX, offsetY;
-	sf::RenderWindow* window;
-};
+#include "Drawable.h"
