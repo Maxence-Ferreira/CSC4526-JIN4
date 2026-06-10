f2f4fd24aa9f9d6721eeb0ba035f1e392e3b33a5
Ferreira Maxence <maxence.ferreira@telecom-sudparis.eu>
2026-06-10 12:33:09 +0200

    building constructor

--- src/core/building/Building.cpp
+++ src/core/building/Building.cpp
@@ -1,5 +1,10 @@
 #include "Building.h"
 
+Building::Building(Ground* tile, int pv_max, int dammage, int range, int cooldown, int price)
+	:m_tile(tile), m_pv_max(pv_max), m_pv(pv_max), m_dammage(dammage), m_range(range), m_cooldown(cooldown),m_price(price),m_level(0),m_dead(0),m_cur_cooldown(0),m_target(0)
+{
+}
+
 int Building::getX() const
 {
 	return m_tile->getX();
@@ -8,6 +13,11 @@ int Building::getX() const
 int Building::getY() const
 {
 	return m_tile->getY();
+}
+
+bool Building::isAlive() const
+{
+	return !m_dead;
 }
 
 int Building::distanceTo(Tile* nei) const
--- src/core/building/Building.h
+++ src/core/building/Building.h
@@ -1,14 +1,32 @@
 #pragma once
 #include "Drawable.h"
 #include "map/Ground.h"
+#include "enemy/Enemy.h"
 
 class Building : public Drawable{
 public:
-
+	Building(
+		Ground* tile,
+		int pv_max,
+		int dammage,
+		int range,
+		int cooldown,
+		int price);
 	int getX() const;
 	int getY() const;
+	bool isAlive() const;
 	int distanceTo(Tile*) const;
 protected:
 private:
 	Ground* m_tile;
+	Enemy* m_target;
+	int m_pv_max;
+	int m_pv;
+	int m_dammage;
+	int m_range;
+	int m_cooldown;
+	bool m_dead;
+	int m_cur_cooldown;
+	int m_level;
+	int m_price;
 };
