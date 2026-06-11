8b7456c3d2cb68c1e210fbff68c84b1dfbf15d93
Ysée PARENTI <ysee.parenti@telecom-sudparis.eu>
2026-06-11 22:12:14 +0200

    ouai les merge conflicts

--- src/core/building/Canon.cpp
+++ src/core/building/Canon.cpp
@@ -1,30 +1,22 @@
 #include "Canon.h"
 
-Canon::Canon(Ground* g, int price):Building(g,1000,5,1000,1000, price)
-{
+Canon::Canon(Ground* g, int price) : Building(g, 1000, 5, 1000, 1000, price) {}
+
+void Canon::draw(const context& ctx) {
+  sf::Transform t;
+  t.translate(
+      sf::Vector2f(TILE_SIZE * (getX() + .5), TILE_SIZE * (getY() + .5)));
+  ctx.window->draw(g_sprite, t);
+  this->drawAttacks(ctx);
 }
 
-void Canon::draw(const context& ctx)
-{
-	sf::Transform t;
-	t.translate(sf::Vector2f( TILE_SIZE * (getX() + .5),TILE_SIZE * (getY() + .5) ));
-	ctx.window->draw(g_sprite, t);
-<<<<<<< HEAD
-	this->drawAttacks(ctx);
-=======
-	drawAttacks(ctx);
->>>>>>> 28f992553ef4e966652f970826c4278bf9f5d35e
-}
+void Canon::update(const context& ctx) { Building::update(ctx); }
 
-void Canon::update(const context& ctx)
-{ Building::update(ctx);
-}
-static sf::RectangleShape create()
-{
-	sf::RectangleShape r({ TILE_SIZE,TILE_SIZE });
-	static sf::Texture tex("resources/CanonTower.png");
-	r.setOrigin({ TILE_SIZE / 2,TILE_SIZE / 2 });
-	r.setTexture(&tex);
-	return r;
+static sf::RectangleShape create() {
+  sf::RectangleShape r({TILE_SIZE, TILE_SIZE});
+  static sf::Texture tex("resources/CanonTower.png");
+  r.setOrigin({TILE_SIZE / 2, TILE_SIZE / 2});
+  r.setTexture(&tex);
+  return r;
 }
 sf::RectangleShape Canon::g_sprite = create();
