04328a9beb87faedf9bbc71142d411671bcd620f
Ysée PARENTI <ysee.parenti@telecom-sudparis.eu>
2026-06-11 17:15:19 +0200

    ajout canon

---
+++ src/core/building/Canon.h
@@ -0,0 +1,13 @@
+#pragma once 
+#include "Building.h"
+#include "SFML/Graphics.hpp"
+
+class Canon : public Building {
+public:
+	Canon(Ground* g,int price);
+	// H�rit� via Building
+	virtual void draw(const context& ctx) override;
+	virtual void update(const context& ctx) override;
+private:
+	static sf::RectangleShape g_sprite;
+};
