f557749b1f61cb668ce642753f929ba379cac59a
Ferreira Maxence <maxence.ferreira@telecom-sudparis.eu>
2026-06-15 18:53:42 +0200

    fix ut

--- src/test/unitTests.cpp
+++ src/test/unitTests.cpp
@@ -1,12 +1,15 @@
 ﻿#include <gtest/gtest.h>
 
 #include <string>
+#include <random>
 #include "map/Terrain.h"
 #include "map/Path.h"
 #include "map/BeginPath.h"
 #include "map/EndPath.h"
 
 TEST(TERRAIN, PathOK) {
-	Terrain terter(50,50);
+	std::random_device rd;
+	std::mt19937 rand(rd());
+	Terrain terter(50,50,5, rand);
 	terter.m_paths;
 }
